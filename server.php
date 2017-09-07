100914265530c91cbf7dcc395c6c5445fc334a56
rmrhz <rmrhz@tfwno.gf>
2017-09-07 22:33:32 +0800

    Load dotenv in server.php

--- server.php
+++ server.php
@@ -5,6 +5,9 @@ require_once "vendor/autoload.php";
 use Phroute\Phroute\RouteCollector;
 use Illuminate\Database\Capsule\Manager as Capsule;  
 
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 // Database Layer
 $capsule = new Capsule; 
 
